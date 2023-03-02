488cdc56aa630fb493096104f5e05bda09283313
Benson Mwangi <bensonmwangi101@gmail.com>
2023-03-02 09:29:24 +0300

    some changes to the file

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -6,16 +6,15 @@
  *
  * Return: If tree is NULL, the function must return 0, else return node count.
  */
- size_t binary_tree_nodes(const binary_tree_t *tree)
- {
-     size_t nodes = 0;
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t nodes = 0;
 
-     if (tree)
-     {
-         nodes += (tree->left || tree->right) ? 1 : 0;
-         nodes += binary_tree_nodes(tree->left);
-         nodes += binary_tree_nodes(tree->right);
-     }
-     return (nodes);
- }
-
+	if (tree)
+	{
+		nodes += (tree->left || tree->right) ? 1 : 0;
+		nodes += binary_tree_nodes(tree->left);
+		nodes += binary_tree_nodes(tree->right);
+	}
+	return (nodes);
+}
